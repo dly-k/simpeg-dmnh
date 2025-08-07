b2533e57e763a900acbcdff8a08bee3a59ded326
dly-k <fadlyf953@gmail.com>
2025-08-07 15:08:40 +0700

    add something

--- resources/views/pendidikan.blade.php
+++ resources/views/pendidikan.blade.php
@@ -3,12 +3,13 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>SIKEMAH - Editor Kegiatan Pendidikan</title>
+  <title>SIKEMAH - Editor Kegiatan ( Pendidikan )</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/pendidikan.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/pendidikan.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="{{ asset('assets/images/logo.png') }}"/>
 </head>
 <body>
   <div class="sidebar" id="sidebar">
