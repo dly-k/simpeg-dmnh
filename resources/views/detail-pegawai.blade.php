4e9eebe0751eff295779fc6651fc1bb910afeb46
dly-k <fadlyf953@gmail.com>
2025-08-07 13:41:35 +0700

    fix something

--- resources/views/detail-pegawai.blade.php
+++ resources/views/detail-pegawai.blade.php
@@ -6,7 +6,7 @@
     <title>Detail Pegawai - SIKEMAH</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/detail-pns.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/detail-pegawai.css') }}">
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
