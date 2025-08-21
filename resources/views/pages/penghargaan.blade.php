4288a97c01bdaae531be7c7b6261a3f13d5e6c40
Mario Raditya Nugroho <marioraditya47@gmail.com>
2025-08-21 15:39:39 +0700

    update css

--- resources/views/pages/penghargaan.blade.php
+++ resources/views/pages/penghargaan.blade.php
@@ -12,6 +12,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
   <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+   <link rel="stylesheet" href="{{ asset('assets/css/layout.css') }}" />
   <link rel="stylesheet" href="{{ asset('assets/css/penghargaan.css') }}" />
 </head>
 
